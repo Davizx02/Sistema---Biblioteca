6114c6a42359f7e120ff16768b46288e98a8e623
Davizx02 <davi.silva166@aluno.educa.go.gov.br>
2026-09-21 18:01:05 +0000

    implementação objeto LIVRO e Metódo ADICIONAR

---
+++ Biblioteca/data/LivroRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Biblioteca.data
+{
+    public class LivroRepository
+    {
+        private static List<Livro> _tabelaLivros = new List<Livro>()
+        private static int proximoID = 1; // Variável auxiliar
+
+        public void Adcionar(Livro livro) // Metódo: adiciona livros na lista
+        {
+           livro.Id = proximoID++;
+           _tabelaLivros.Add(livro);
+        }
+        
+    }
+}
